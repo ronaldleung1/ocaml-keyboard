0b3683b5ac7bf8dc474aac0daf9ee431f5c4d629
Ronald Leung <rfl68@cornell.edu>
2024-03-28 20:38:57 -0400

    include playing notes in main event loop

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,12 @@
 open Music
 open Domainslib
+
 let notes = [ 'A'; 'B'; 'C'; 'D'; 'E'; 'F'; 'G' ]
 
 let print_notes pressed_note =
   List.iter
     (fun note ->
-      if Char.equal note pressed_note then
+      if Char.equal note (Char.uppercase_ascii pressed_note) then
         ANSITerminal.print_string
           [ ANSITerminal.green; ANSITerminal.on_black ]
           (String.make 1 note)
@@ -13,41 +14,52 @@ let print_notes pressed_note =
         ANSITerminal.print_string
           [ ANSITerminal.white; ANSITerminal.on_black ]
           (String.make 1 note))
-    notes
+    notes;
+  print_newline ()
 
 let () =
   print_endline "Welcome to Caml Music!";
-  print_endline "Enter a note (A-G):";
-  let input = read_line () in
-  if String.length input > 0 then
-    let pressed_note = input.[0] in
-    print_notes pressed_note
-  else print_endline "No note entered";
-  print_newline ();
   print_string "Enter a beats per minute for your metronome: ";
   let bpm = read_line () in
-  Metronome.set_bpm (float_of_int ((int_of_string) bpm));
-  print_newline ()
+  Metronome.set_bpm (float_of_int (int_of_string bpm));
+  print_endline
+    "Enter a note (A-G), or enter the tempo for the metronome (in \
+     beats per minute):";
+  print_notes ' '
 
+let play_note note =
+  if String.length note = 1 then
+    let pressed_note = note.[0] in
+    print_notes pressed_note
+  else print_endline "Invalid input"
+
+(* main event loop *)
 let listen_for_commands () =
   while Atomic.get Metronome.is_on do
     let input = read_line () in
     if input = "stop" then Atomic.set Metronome.is_on false
-    else Metronome.set_bpm (float_of_string input)
+    else
+      match float_of_string_opt input with
+      | None -> play_note input
+      | Some x -> Metronome.set_bpm x
   done
 
-let metronome_commands pool () = 
-  let start = Task.async pool (fun _ -> Metronome.start_metronome (Unix.gettimeofday() +. 1.) ()) in
+let metronome_commands pool () =
+  let start =
+    Task.async pool (fun _ ->
+        Metronome.start_metronome (Unix.gettimeofday () +. 1.) ())
+  in
   let listener = Task.async pool (fun _ -> listen_for_commands ()) in
-  let _ = Task.await pool start in 
-  let _ = Task.await pool listener in ()
+  let _ = Task.await pool start in
+  let _ = Task.await pool listener in
+  ()
 
-
-let () = 
+(* initialize *)
+let () =
   let pool = Task.setup_pool ~num_domains:4 () in
-  print_endline "Starting your metronome... (type 'stop' to stop and an integer to change the bpm)";
+  print_endline
+    "Starting your metronome... (type 'stop' to stop and an integer to \
+     change the bpm)";
   let _ = Task.run pool (fun () -> metronome_commands pool ()) in
   (* Task.teardown_pool pool; *)
-  print_endline "Metronome stopped.";
-
-
+  print_endline "Metronome stopped."
