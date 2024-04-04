7affea9f69ab02aa3d31d11b2053f615ae1d60f2
Ronald Leung <ronaldfleung@gmail.com>
2024-04-03 21:26:58 -0400

    adjust metronome speed and toggle on/off

--- lib/metronome.ml
+++ lib/metronome.ml
@@ -1,8 +1,20 @@
-let start bpm =
-  let tick = Raylib.load_sound "assets/metronome-trimmed.wav" in
-  let interval = 60. /. bpm in
-  let next_time = ref (Unix.gettimeofday () +. interval) in
+open Raylib
+
+let start (start_bpm : float) =
+  let tick = load_sound "assets/metronome-trimmed.wav" in
+  let bpm = ref start_bpm in
+  let next_time = ref (Unix.gettimeofday ()) in
+  let running = ref true in
   fun () ->
-    if Unix.gettimeofday () >= !next_time then (
-      Raylib.play_sound tick;
-      next_time := Unix.gettimeofday () +. interval)
+    draw_text
+      (String.sub (string_of_float !bpm) 0
+         (String.length (string_of_float !bpm) - 1))
+      100 100 16 Raylib.Color.gold;
+    if !running then (
+      if is_key_down Key.Up then bpm := !bpm +. 1.0
+      else if is_key_down Key.Down then bpm := !bpm -. 1.0
+      else if is_key_pressed Key.Space then running := false
+      else if Unix.gettimeofday () >= !next_time then (
+        play_sound tick;
+        next_time := Unix.gettimeofday () +. (60. /. !bpm)))
+    else if is_key_pressed Key.Space then running := true
