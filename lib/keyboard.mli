66ba8f5083d1b3f2cb85a96c95d3af133f01f0b7
Justin Xiang <jx372@cornell.edu>
2024-04-29 13:00:56 -0400

    Fix documentation

--- lib/keyboard.mli
+++ lib/keyboard.mli
@@ -3,7 +3,7 @@ val curr_octave : int ref
 
 val init_keyboard :
   int -> Raylib.Rectangle.t -> string -> (unit -> unit) list
-(** [init_keyboard rect init_octave instrument] draws the keyboard
+(** [init_keyboard init_octave rect instrument] draws the keyboard
     forand creates the corresponding keyboard buttons for the specific
     instrument. The keyboard is limited to keys in the init_octave.
     Requires init_octave to be in [0, 7]. *)
