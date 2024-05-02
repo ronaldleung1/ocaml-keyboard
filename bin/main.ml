97e1283d00b88df66014a7159a8bc0aa4ee8d62c
Justin Xiang <jx372@cornell.edu>
2024-05-02 17:37:17 -0400

    Read instruments from CSV

--- bin/main.ml
+++ bin/main.ml
@@ -5,138 +5,23 @@ let screenWidth = 800
 let screenHeight = 450
 let argv : string list = Array.to_list Sys.argv
 
-let instruments =
-  [
-    ("piano", create_instrument "piano");
-    ("accordion", create_instrument "accordion");
-    ("acoustic_bass", create_instrument "acoustic_bass");
-    ("acoustic_grand_piano", create_instrument "acoustic_grand_piano");
-    ("acoustic_guitar_nylon", create_instrument "acoustic_guitar_nylon");
-    ("acoustic_guitar_steel", create_instrument "acoustic_guitar_steel");
-    ("agogo", create_instrument "agogo");
-    ("alto_sax", create_instrument "alto_sax");
-    ("applause", create_instrument "applause");
-    ("bagpipe", create_instrument "bagpipe");
-    ("banjo", create_instrument "banjo");
-    ("baritone_sax", create_instrument "baritone_sax");
-    ("bassoon", create_instrument "bassoon");
-    ("bird_tweet", create_instrument "bird_tweet");
-    ("blown_bottle", create_instrument "blown_bottle");
-    ("brass_section", create_instrument "brass_section");
-    ("breath_noise", create_instrument "breath_noise");
-    ("bright_acoustic_piano", create_instrument "bright_acoustic_piano");
-    ("celesta", create_instrument "celesta");
-    ("cello", create_instrument "cello");
-    ("choir_aahs", create_instrument "choir_aahs");
-    ("church_organ", create_instrument "church_organ");
-    ("clarinet", create_instrument "clarinet");
-    ("clavinet", create_instrument "clavinet");
-    ("contrabass", create_instrument "contrabass");
-    ("distortion_guitar", create_instrument "distortion_guitar");
-    ("drawbar_organ", create_instrument "drawbar_organ");
-    ("dulcimer", create_instrument "dulcimer");
-    ("electric_bass_finger", create_instrument "electric_bass_finger");
-    ("electric_bass_pick", create_instrument "electric_bass_pick");
-    ("electric_grand_piano", create_instrument "electric_grand_piano");
-    ("electric_guitar_clean", create_instrument "electric_guitar_clean");
-    ("electric_guitar_jazz", create_instrument "electric_guitar_jazz");
-    ("electric_guitar_muted", create_instrument "electric_guitar_muted");
-    ("electric_piano_1", create_instrument "electric_piano_1");
-    ("electric_piano_2", create_instrument "electric_piano_2");
-    ("english_horn", create_instrument "english_horn");
-    ("fiddle", create_instrument "fiddle");
-    ("flute", create_instrument "flute");
-    ("french_horn", create_instrument "french_horn");
-    ("fretless_bass", create_instrument "fretless_bass");
-    ("fx_1_rain", create_instrument "fx_1_rain");
-    ("fx_2_soundtrack", create_instrument "fx_2_soundtrack");
-    ("fx_3_crystal", create_instrument "fx_3_crystal");
-    ("fx_4_atmosphere", create_instrument "fx_4_atmosphere");
-    ("fx_5_brightness", create_instrument "fx_5_brightness");
-    ("fx_6_goblins", create_instrument "fx_6_goblins");
-    ("fx_7_echoes", create_instrument "fx_7_echoes");
-    ("fx_8_scifi", create_instrument "fx_8_scifi");
-    ("glockenspiel", create_instrument "glockenspiel");
-    ("guitar_fret_noise", create_instrument "guitar_fret_noise");
-    ("guitar_harmonics", create_instrument "guitar_harmonics");
-    ("gunshot", create_instrument "gunshot");
-    ("harmonica", create_instrument "harmonica");
-    ("harpsichord", create_instrument "harpsichord");
-    ("helicopter", create_instrument "helicopter");
-    ("honkytonk_piano", create_instrument "honkytonk_piano");
-    ("kalimba", create_instrument "kalimba");
-    ("koto", create_instrument "koto");
-    ("lead_1_square", create_instrument "lead_1_square");
-    ("lead_2_sawtooth", create_instrument "lead_2_sawtooth");
-    ("lead_3_calliope", create_instrument "lead_3_calliope");
-    ("lead_4_chiff", create_instrument "lead_4_chiff");
-    ("lead_5_charang", create_instrument "lead_5_charang");
-    ("lead_6_voice", create_instrument "lead_6_voice");
-    ("lead_7_fifths", create_instrument "lead_7_fifths");
-    ("lead_8_bass__lead", create_instrument "lead_8_bass__lead");
-    ("marimba", create_instrument "marimba");
-    ("melodic_tom", create_instrument "melodic_tom");
-    ("music_box", create_instrument "music_box");
-    ("muted_trumpet", create_instrument "muted_trumpet");
-    ("oboe", create_instrument "oboe");
-    ("ocarina", create_instrument "ocarina");
-    ("orchestra_hit", create_instrument "orchestra_hit");
-    ("orchestral_harp", create_instrument "orchestral_harp");
-    ("overdriven_guitar", create_instrument "overdriven_guitar");
-    ("pad_1_new_age", create_instrument "pad_1_new_age");
-    ("pad_2_warm", create_instrument "pad_2_warm");
-    ("pad_3_polysynth", create_instrument "pad_3_polysynth");
-    ("pad_4_choir", create_instrument "pad_4_choir");
-    ("pad_5_bowed", create_instrument "pad_5_bowed");
-    ("pad_6_metallic", create_instrument "pad_6_metallic");
-    ("pad_7_halo", create_instrument "pad_7_halo");
-    ("pad_8_sweep", create_instrument "pad_8_sweep");
-    ("pan_flute", create_instrument "pan_flute");
-    ("percussive_organ", create_instrument "percussive_organ");
-    ("piccolo", create_instrument "piccolo");
-    ("pizzicato_strings", create_instrument "pizzicato_strings");
-    ("recorder", create_instrument "recorder");
-    ("reed_organ", create_instrument "reed_organ");
-    ("reverse_cymbal", create_instrument "reverse_cymbal");
-    ("rock_organ", create_instrument "rock_organ");
-    ("seashore", create_instrument "seashore");
-    ("shakuhachi", create_instrument "shakuhachi");
-    ("shamisen", create_instrument "shamisen");
-    ("shanai", create_instrument "shanai");
-    ("sitar", create_instrument "sitar");
-    ("slap_bass_1", create_instrument "slap_bass_1");
-    ("slap_bass_2", create_instrument "slap_bass_2");
-    ("soprano_sax", create_instrument "soprano_sax");
-    ("steel_drums", create_instrument "steel_drums");
-    ("string_ensemble_1", create_instrument "string_ensemble_1");
-    ("string_ensemble_2", create_instrument "string_ensemble_2");
-    ("synth_bass_1", create_instrument "synth_bass_1");
-    ("synth_bass_2", create_instrument "synth_bass_2");
-    ("synth_brass_1", create_instrument "synth_brass_1");
-    ("synth_brass_2", create_instrument "synth_brass_2");
-    ("synth_choir", create_instrument "synth_choir");
-    ("synth_drum", create_instrument "synth_drum");
-    ("synth_strings_1", create_instrument "synth_strings_1");
-    ("synth_strings_2", create_instrument "synth_strings_2");
-    ("taiko_drum", create_instrument "taiko_drum");
-    ("tango_accordion", create_instrument "tango_accordion");
-    ("telephone_ring", create_instrument "telephone_ring");
-    ("tenor_sax", create_instrument "tenor_sax");
-    ("timpani", create_instrument "timpani");
-    ("tinkle_bell", create_instrument "tinkle_bell");
-    ("tremolo_strings", create_instrument "tremolo_strings");
-    ("trombone", create_instrument "trombone");
-    ("trumpet", create_instrument "trumpet");
-    ("tuba", create_instrument "tuba");
-    ("tubular_bells", create_instrument "tubular_bells");
-    ("vibraphone", create_instrument "vibraphone");
-    ("viola", create_instrument "viola");
-    ("violin", create_instrument "violin");
-    ("voice_oohs", create_instrument "voice_oohs");
-    ("whistle", create_instrument "whistle");
-    ("woodblock", create_instrument "woodblock");
-    ("xylophone", create_instrument "xylophone");
-  ]
+let load_instruments_from_csv file_path =
+  let ic = open_in file_path in
+  let rec read_lines acc =
+    try
+      let line = input_line ic in
+      let instrument = String.trim line in
+      if instrument <> "" then
+        read_lines ((instrument, create_instrument instrument) :: acc)
+      else
+        acc
+    with End_of_file ->
+      close_in ic;
+      List.rev acc
+  in
+  read_lines []
+
+let instruments = load_instruments_from_csv "assets/instruments.csv"
 
 let valid_instrument_names = List.map fst instruments
 let list_view_scroll_index = ref 0
